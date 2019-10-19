fd262235bffd4d36db72a466e0dadc76d132c9d2
Matt Bechtel <bechma02@gmail.com>
2019-10-19 01:16:08 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -29,3 +29,19 @@ end
 def using_insert(array,new_element)
   array.insert(4,new_element)
 end
+
+def using_uniq(array)
+  array.uniq
+end
+
+def using_flatten(array)
+  array.flatten
+end
+
+def using_delete(array,string)
+  array.delete(string)
+end
+
+def using_delete_at(array,integer)
+  array.delete_at(integer)
+end
